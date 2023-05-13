b20a586e54b636fcaa38c6cf436250955b2c28a8
Christian Menendez Luttringer <luttringerezequiel@gmail.com>
2023-05-13 07:49:26 -0300

    se mejora widget de producto, ahora recibe codigo producto e implementa toda la logica desde una bd (array con objetos). se implementa contador

--- react_app_v2/src/components/CartWidget/CartWidget.jsx
+++ react_app_v2/src/components/CartWidget/CartWidget.jsx
@@ -1,34 +1,48 @@
-import "./CartWidget.css"
+import "./CartWidget.css";
+import { useState } from "react";
+
 const CartWidget = (props) => 
 {
-  let {nombreP,descP,precioP,imgP} = props;
-
-  const bd_items_stock =  //codigo,nombre, descripcion, precio, imgURL, stock
+  let {codigoProducto} = props; 
+  const bd_items_stock =  //codigo,nombre, descripcion, precio, imgURL, stock, inicialStock
   [
-    ["#a0001","Vino Tinto Tannat Cabernet Gran Guarda H. STAGNARI 750 ml", "sin descripcion de momento.",449, "https://prod-resize.tiendainglesa.com.uy/images/large/P387628-2.jpg?20220505151608,Vino-Tinto-Tannat-Cabernet-Gran-Guarda-H.-STAGNARI-750-ml-en-Tienda-Inglesa", 35],
-    ["#a0002","Vino Tinto Malbec Cafayate Reserve BODEGA ETCHART 750 ml", "Color rojo violáceo intenso con reflejos púrpuras.Olfato aromático,con notas de ciruela madura, uvas pasas, especias dulces y tostadas de su crianza en roble. Ideal para acompañar carnes vacunas o de cerdo.Temperatura de servicio 15-17°C.", 435, "https://prod-resize.tiendainglesa.com.uy/images/large/P398380-2.jpg?20210504132800,Vino-Tinto-Malbec-Cafayate-Reserve-BODEGA-ETCHART-750-ml-en-Tienda-Inglesa", 33],
-    ["#a0003","Vino Tinto JUAN CARRAU Cabernet Sauvignon 750 ml", "Frutos rojos en armonía con notas herbáceas son evidentes en el aroma.Seco y redondo, muy agradable al paladar. Ideal para acompañar todo tipo de carnes, pastas y fiambres.Se recomienda en el verano servir fresco a 18º C.", 436, "https://prod-resize.tiendainglesa.com.uy/images/large/P004596-1.jpg?20210701173132,Vino-Tinto-JUAN-CARRAU-Cabernet-Sauvignon-750-ml-en-Tienda-Inglesa", 25],
-    ["#a0004","Vino Tinto TRAPICHE Reserva Malbec 750 ml", "De color violáceo, ofrece a la nariz pain grillé, mineral, ciruela y guindas con un toque de pimiento negro. Al paladar es suave, redondo, maduro y con sabores a bayas. El final es especiado, con amplios taninos masticables.", 540, "https://prod-resize.tiendainglesa.com.uy/images/large/P094600-1.jpg?20210701173132,Vino-Tinto-TRAPICHE-Reserva-Malbec-750-ml-en-Tienda-Inglesa", 56],
-    ["#a0005","Vino Tinto CONCHA Y TORO Cabernet Sauvignon Reservado 750 ml", "Color Rojo rubí, con notas violáceas. Aroma Aroma frutal con notas a ciruelas rojas, chocolate y un leve toque vegetal.", 540, "https://prod-resize.tiendainglesa.com.uy/images/large/P094600-1.jpg?20210701173132,Vino-Tinto-TRAPICHE-Reserva-Malbec-750-ml-en-Tienda-Inglesa",840],
-    ["#a0006","Vino MASSIMO DEICAS Tinto Tannat 750 ml", "Color, es un vino que mantiene color profundo a pesar del paso del tiempo. Nariz: es donde su complejidad alcanza su plenitud, frutos negros ,casis , regaliz , higos en su tinta, se expresan sobre un fondo de madera fina y elegante. Boca potente con intensidad en taninos muy maduros que aportan estrucura y describen un gran cuerpo, una acidez viva asegura una noble y lento envejecimiento para este majestuoso tannat uruguayo.", 3353, "https://prod-resize.tiendainglesa.com.uy/images/large/P217435-1.jpg?20181119182803,Vino-MASSIMO-DEICAS-Tinto-Tannat-750-ml-en-Tienda-Inglesa", 53],
-    ["#a0007","Vino Tinto Dinastia H. STAGNARI 750ml", "sin descripcion de momento.",1890,"https://prod-resize.tiendainglesa.com.uy/images/large/P140991-1.jpg?20190729125546,Vino-Tinto-Dinastia-H.-STAGNARI-750ml-en-Tienda-Inglesa",34],
-    ["#a0008","Vino Tannat Marselan Garra H. STAGNARI", "sin descripcion de momento.", 565, "https://prod-resize.tiendainglesa.com.uy/images/large/P496743-2.jpg?20220505144931,Vino-Tannat-Marselan-Garra-H.-STAGNARI-en-Tienda-Inglesa", 87]
+    {codigoProducto:"#a0001", nombreProducto:"Vino Tinto Tannat Cabernet Gran Guarda H. STAGNARI 750 ml", descriProducto:"sin descripcion de momento.",precioProducto:449, urlimgProducto:"https://prod-resize.tiendainglesa.com.uy/images/large/P387628-2.jpg?20220505151608,Vino-Tinto-Tannat-Cabernet-Gran-Guarda-H.-STAGNARI-750-ml-en-Tienda-Inglesa", stockProducto:35, inicialStock:1},
+    {codigoProducto:"#a0002", nombreProducto:"Vino Tinto Malbec Cafayate Reserve BODEGA ETCHART 750 ml", descriProducto:"Color rojo violáceo intenso con reflejos púrpuras.Olfato aromático,con notas de ciruela madura, uvas pasas, especias dulces y tostadas de su crianza en roble. Ideal para acompañar carnes vacunas o de cerdo.Temperatura de servicio 15-17°C.", precioProducto:435, urlimgProducto:"https://prod-resize.tiendainglesa.com.uy/images/large/P398380-2.jpg?20210504132800,Vino-Tinto-Malbec-Cafayate-Reserve-BODEGA-ETCHART-750-ml-en-Tienda-Inglesa", stockProducto:33, inicialStock:1},
+    {codigoProducto:"#a0003", nombreProducto:"Vino Tinto JUAN CARRAU Cabernet Sauvignon 750 ml", descriProducto:"Frutos rojos en armonía con notas herbáceas son evidentes en el aroma.Seco y redondo, muy agradable al paladar. Ideal para acompañar todo tipo de carnes, pastas y fiambres.Se recomienda en el verano servir fresco a 18º C.", precioProducto:436, urlimgProducto:"https://prod-resize.tiendainglesa.com.uy/images/large/P004596-1.jpg?20210701173132,Vino-Tinto-JUAN-CARRAU-Cabernet-Sauvignon-750-ml-en-Tienda-Inglesa", stockProducto:25, inicialStock:1},
+    {codigoProducto:"#a0004", nombreProducto:"Vino Tinto TRAPICHE Reserva Malbec 750 ml", descriProducto:"De color violáceo, ofrece a la nariz pain grillé, mineral, ciruela y guindas con un toque de pimiento negro. Al paladar es suave, redondo, maduro y con sabores a bayas. El final es especiado, con amplios taninos masticables.", precioProducto:540, urlimgProducto:"https://prod-resize.tiendainglesa.com.uy/images/large/P094600-1.jpg?20210701173132,Vino-Tinto-TRAPICHE-Reserva-Malbec-750-ml-en-Tienda-Inglesa", stockProducto:56, inicialStock:1},
+    {codigoProducto:"#a0005", nombreProducto:"Vino Tinto CONCHA Y TORO Cabernet Sauvignon Reservado 750 ml", descriProducto:"Color Rojo rubí, con notas violáceas. Aroma Aroma frutal con notas a ciruelas rojas, chocolate y un leve toque vegetal.", precioProducto:540, urlimgProducto:"https://prod-resize.tiendainglesa.com.uy/images/large/P094600-1.jpg?20210701173132,Vino-Tinto-TRAPICHE-Reserva-Malbec-750-ml-en-Tienda-Inglesa", stockProducto:840, inicialStock:1},
+    {codigoProducto:"#a0006", nombreProducto:"Vino MASSIMO DEICAS Tinto Tannat 750 ml", descriProducto:"Color, es un vino que mantiene color profundo a pesar del paso del tiempo. Nariz: es donde su complejidad alcanza su plenitud, frutos negros ,casis , regaliz , higos en su tinta, se expresan sobre un fondo de madera fina y elegante. Boca potente con intensidad en taninos muy maduros que aportan estrucura y describen un gran cuerpo, una acidez viva asegura una noble y lento envejecimiento para este majestuoso tannat uruguayo.", precioProducto:3353, urlimgProducto:"https://prod-resize.tiendainglesa.com.uy/images/large/P217435-1.jpg?20181119182803,Vino-MASSIMO-DEICAS-Tinto-Tannat-750-ml-en-Tienda-Inglesa", stockProducto:53, inicialStock:1},
+    {codigoProducto:"#a0007", nombreProducto:"Vino Tinto Dinastia H. STAGNARI 750ml", descriProducto:"sin descripcion de momento.", precioProducto:1890, urlimgProducto:"https://prod-resize.tiendainglesa.com.uy/images/large/P140991-1.jpg?20190729125546,Vino-Tinto-Dinastia-H.-STAGNARI-750ml-en-Tienda-Inglesa", stockProducto:34, inicialStock:1},
+    {codigoProducto:"#a0008", nombreProducto:"Vino Tannat Marselan Garra H. STAGNARI", descriProducto:"sin descripcion de momento.", precioProducto:565, urlimgProducto:"https://prod-resize.tiendainglesa.com.uy/images/large/P496743-2.jpg?20220505144931,Vino-Tannat-Marselan-Garra-H.-STAGNARI-en-Tienda-Inglesa", stockProducto:87, inicialStock:1},
   ];
+  const obj_producto = bd_items_stock.find((producto) => producto.codigoProducto === codigoProducto);
+  const [contador, setContador] = useState(obj_producto.inicialStock);  //estamos desestructurando la funcion de retorno del useState. nos devuelve valor (que es contador) y una funcion para setearlo.
+  
+
+  const incrementarCounter = () => {contador<obj_producto.stockProducto && setContador(contador+1);}
+
+  const disminuirCounter = () => {contador>obj_producto.inicialStock && setContador(contador-1);}
+
+  const agregarCarrito = () =>
+  {
+    console.log(`Agregado ${contador} items de: ${obj_producto.nombreProducto}. total compra: $${contador*obj_producto.precioProducto} pesos uruguayos.`); //funcion que avisa se agrego a carrito x cantidad de un item, hay que agregar que item es.
+  }
+
 
   return (
     <div class="productoItem">
-        <img src={imgP} alt="" />
-        <h4>{nombreP}</h4>
-        <p>{descP}</p>
-        <p>{precioP}</p>
+        <img src={obj_producto.urlimgProducto} alt="" />
+        <h4>{obj_producto.nombreProducto}</h4>
+        {/*<p>{obj_producto.descriProducto}</p>   -- mucho texto*/}
+        <p>{"$" + obj_producto.precioProducto}</p>
 
         <div>
-          <button>-</button>
-          <input type="text" value="1"/>
-          <button>+</button>
+          <button onClick={disminuirCounter}>-</button>
+          <input type="text" value={contador} disabled/>
+          <button onClick={incrementarCounter}>+</button>
         </div>
         
-        <button>agregar a carrito</button>
+        <button onClick={agregarCarrito}>agregar a carrito</button>
     </div>
   );
 }
--- react_app_v2/src/components/ShopCont/ShopCont.jsx
+++ react_app_v2/src/components/ShopCont/ShopCont.jsx
@@ -5,14 +5,14 @@ import './ShopCont.css';
 const ShopCont = () => {
   return (
     <div class="ShopContenedor">
-      <CartWidget nombreP="Yerba Serena CANARIAS 1 Kg" descP="yerba Canarias con un fino equilibrio de las hierbas" precioP="213" imgP="https://prod-resize.tiendainglesa.com.uy/images/large/P042940-1.jpg?20220111155842,Yerba-Serena-CANARIAS-1-Kg-en-Tienda-Inglesa"/>
-      <CartWidget nombreP="Agua Mineral Natural sin Gas SALUS 6.25 L" descP="Agua Salus identidad de los uruguayos, con más de 120 años de trayectoria." precioP="129" imgP="https://prod-resize.tiendainglesa.com.uy/images/large/P113618-1.jpg?20230118210021,Agua-Mineral-Natural-sin-Gas-SALUS-6.25-L-en-Tienda-Inglesa"/>
-      <CartWidget nombreP="Nuggets de Pollo Crocantes SADIA 900 gr" descP="sin informacion adicional" precioP="315" imgP="https://prod-resize.tiendainglesa.com.uy/images/large/P414560-1.jpg?20200203133814,Nuggets-de-Pollo-Crocantes-SADIA-900-gr-en-Tienda-Inglesa"/>
-      <CartWidget nombreP="Palta Hass (por Unidad)" descP="Fruta de excelente calidad, sin fibra, alta resistencia al transporte y larga vida poscosecha." precioP="55" imgP="https://prod-resize.tiendainglesa.com.uy/images/large/P032248-1.jpg?20221104104756,Palta-Hass-por-Unidad-en-Tienda-Inglesa"/>
-      <CartWidget nombreP="Arroz BLUE PATNA 1 Kg" descP="sin informacion adicional" precioP="67" imgP="https://prod-resize.tiendainglesa.com.uy/images/large/P000208-1.jpg?20191127105540,Arroz-BLUE-PATNA-1-Kg-en-Tienda-Inglesa"/>
-      <CartWidget nombreP="El Azúcar Blanco Refinado es obtenido a partir de la plantación de caña de azúcar en Bella Unión." descP="El Azúcar Blanco Refinado es obtenido a partir de la plantación de caña de azúcar en Bella Unión." precioP="57" imgP="https://prod-resize.tiendainglesa.com.uy/images/large/P000163-1.jpg?20200709110051,Azucar-BELLA-UNI%C3%93N-Blanco-1Kg-en-Tienda-Inglesa"/>
-      <CartWidget nombreP="Dulce de Leche CONAPROLE Clásico 500 gr" descP="ings:Leche entera pasteurizada, azúcar, almidón de maíz, conservador: sorbato de potasio (INS 202). Apto para celíacos." precioP="138" imgP="https://prod-resize.tiendainglesa.com.uy/images/large/P014587-1.jpg?20220705194926,Dulce-de-Leche-CONAPROLE-Clasico-500-gr-en-Tienda-Inglesa"/>
-      <CartWidget nombreP="Vino Tinto CONCHA Y TORO Cabernet Sauvignon Reservado 750 ml" descP="Color Rojo rubí, con notas violáceas. Aroma Aroma frutal con notas a ciruelas rojas, chocolate y un leve toque vegetal." precioP="298" imgP="https://prod-resize.tiendainglesa.com.uy/images/large/P067225-1.jpg?20210701173132,Vino-Tinto-CONCHA-Y-TORO-Cabernet-Sauvignon-Reservado-750-ml-en-Tienda-Inglesa"/>
+      <CartWidget codigoProducto="#a0001"/>
+      <CartWidget codigoProducto="#a0002"/>
+      <CartWidget codigoProducto="#a0003"/>
+      <CartWidget codigoProducto="#a0004"/>
+      <CartWidget codigoProducto="#a0005"/>
+      <CartWidget codigoProducto="#a0006"/>
+      <CartWidget codigoProducto="#a0007"/>
+      <CartWidget codigoProducto="#a0008"/>
     </div>
   );
 }
